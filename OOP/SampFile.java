c325f8823bc73352f807b23749cdbe360fdd2513
jushiiko <jsAblay@mcm.edu.ph>
2022-08-08 15:40:10 +0800

    init commit

---
+++ OOP/SampFile.java
@@ -0,0 +1,17 @@
+package OOP;
+
+public class SampFile {
+    public static void main(String[] args) {
+        int sum = 0;
+        int dif = 1;
+        int a,b;
+        double c;
+        char j;
+        char l = '*';
+        j = '$';
+        a = 10;
+        System.out.println("Hello");
+        System.out.println("Good Afternoon");
+
+    }
+}
