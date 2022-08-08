0865860531c6bf2e75c234e9fa4eff2a45b5652d
jushiiko <jsAblay@mcm.edu.ph>
2022-08-08 08:41:02 +0800

    init commit

--- DSA/FirstProgram.java
+++ DSA/FirstProgram.java
@@ -2,8 +2,31 @@ package DSA;
 
 public class FirstProgram {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        //Primitive Type - stack memory
+        //Arrays - heap memory
+    int num[]= {23,45,67,100,90};
+    System.out.println(num.length);
+    num[2]=45;
+    System.out.println(num[3]);
 
+    for(int i=0;i<num.length;i++)
+    {
+        System.out.println(num[i]+" ");
+    }
+    FirstProgram p=new FirstProgram();
+    int highest=p.max(num);
+    System.out.println(highest);
     }
     
+    }
+    public int max(int num[] ) {
+        return 0;
+    }
+
+    public void sum (int num[] ) {
+    
+    }
+
+
+
 }
