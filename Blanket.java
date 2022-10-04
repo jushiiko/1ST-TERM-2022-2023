5dbd2fc5881aa8251c8cf21fdd08ee1bbefa1562
jushiiko <jsAblay@mcm.edu.ph>
2022-10-04 16:08:30 +0800

    init commit

---
+++ Blanket.java
@@ -0,0 +1,80 @@
+public class Blanket
+{
+    protected String size;
+    protected String color;
+    protected String material;
+    protected double price;
+    protected double sizePremium;
+    protected double materialPremium;
+    private final String SIZE1 = "Twin";
+    private final String SIZE2 = "Double";
+    private final String SIZE3 = "Queen";
+    private final String SIZE4 = "King";
+    protected final double BASE_PRICE = 30;
+    private final double S_PRICE2 = 10;
+    private final double S_PRICE3 = 25;
+    private final double S_PRICE4 = 40;
+    private final String MAT1 = "cotton";
+    private final String MAT2 = "wool";
+    private final String MAT3 = "cashmere";
+    private final double M_PRICE2 = 20;
+    private final double M_PRICE3 = 45;
+    public Blanket()
+    {
+        setDefaults();
+    }
+    private void setDefaults()
+    {
+        // write your code here
+        size = SIZE1;
+        color = "white";
+        material = MAT1;
+        price = BASE_PRICE;
+        sizePremium = 0;
+        materialPremium = 0;
+    }
+    public String getSize()
+    {
+        // write your code here
+        return size;
+    }
+    public String getColor()
+    {
+        // write your code here
+        return color;
+    }
+    public String getMaterial()
+    {
+        // write your code here
+        return material;
+    }
+    public void setSize(String s)
+    {
+        // write your code here
+        
+    }
+    public void setMaterial(String m)
+    {
+        // write your code here
+        material = m;
+        if (material.equals(MAT1))
+        {
+            materialPremium = 0;
+        }
+        else if (material.equals(MAT2))
+        {
+            materialPremium = M_PRICE2;
+        }
+        else if (material.equals(MAT3))
+        {
+            materialPremium = M_PRICE3;
+        }
+    }
+    public String toString()
+    {
+        // write your code here
+        return "Size: " + size + "" + "Color: " + color + "" + 
+        "Material: " + material + "" + "Price: " + price;
+
+    }
+}
---
+++ ElectricBlanket.java
@@ -0,0 +1,50 @@
+public class ElectricBlanket extends Blanket
+{
+    private int settings;
+    private boolean hasAutoShutoff;
+    private final int MAX_SETTINGS = 5;
+    private double shutoffPremium;
+    private final double S_PRICE = 5.75;
+    public ElectricBlanket()
+    {
+        // write your code here
+        settings = 1;
+        hasAutoShutoff = false;
+        shutoffPremium = 0;
+    }
+    public int getSettings()
+    {
+        // write your code here
+        return settings;
+    }
+    public boolean getHasAutoShutoff()
+    {
+        // write your code here
+        return hasAutoShutoff;
+    }
+    public void setSettings(int s)
+    {
+        // write your code here
+        settings = s;
+    }
+    public void setHasAutoShutoff(boolean h)
+    {
+        // write your code here
+        hasAutoShutoff = h;
+        if (hasAutoShutoff)
+        {
+            shutoffPremium = S_PRICE;
+        }
+        else
+        {
+            shutoffPremium = 0;
+        }
+    }
+    public String toString()
+    {
+        // write your code here
+        return "Size: " + size + "" + "Color: " + color + "" + "Material: " + material + 
+        "" + "Price: " + price + "" + "Settings: " + settings + "" + "Has Auto Shutoff: " + hasAutoShutoff;
+    }
+} 
+
