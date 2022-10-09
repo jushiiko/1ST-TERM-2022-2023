e6b56b9c9c73bae3a666dc04ea546c512cb2a20b
jushiiko <jsAblay@mcm.edu.ph>
2022-10-10 00:50:55 +0800

    init commit

--- ExpensesCalculator.java
+++ ExpensesCalculator.java
@@ -2,12 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-
 import javax.swing.JFrame;
 
 /**
  *
- * @author matae
+ * @author Ablay, John Sebastian C.
+ * @author Marquez, Marc Steven Q.
+ * @author Panggoy, Julia Coleene B.
  */
 public class ExpensesCalculator {
 
@@ -16,11 +17,12 @@ public class ExpensesCalculator {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
         ExpensesCalculatorGUI jgui = new ExpensesCalculatorGUI();
         jgui.setVisible(true);
         jgui.pack();
         jgui.setLocationRelativeTo(null);
         jgui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-    
+
 }
